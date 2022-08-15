3573099b952fff2ea8e4ca62766ccd62cc70dee0
Nathan Rignall <rignallnathan@gmail.com>
2022-08-16 00:00:06 +0100

    Set lambda to use middleware

--- apps/api/src/lambda.ts
+++ apps/api/src/lambda.ts
@@ -1,10 +1,12 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { Server } from 'http';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as awsServerlessExpress from 'aws-serverless-express';
 import * as express from 'express';
+import * as cookieParser from 'cookie-parser';
 
 let cachedServer: Server;
 
@@ -12,7 +14,12 @@ const bootstrapServer = async (): Promise<Server> => {
   const expressApp = express();
   const adapter = new ExpressAdapter(expressApp);
   const app = await NestFactory.create(AppModule, adapter);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
+
   app.enableCors();
+
   await app.init();
   return awsServerlessExpress.createServer(expressApp);
 };
