3851dd57bcbda4e5839c2ca57512bca28c40817e
Nathan Rignall <rignallnathan@gmail.com>
2022-08-20 18:10:14 +0100

    Navbar UI updates

--- apps/web/src/modules/containers/Navbar/navbar.tsx
+++ apps/web/src/modules/containers/Navbar/navbar.tsx
@@ -9,7 +9,7 @@ export function Navbar() {
   const [menu, toggleMenu] = useToggle(false);
 
   return (
-    <div className="fixed w-full border-b-2 bg-white">
+    <div className="fixed w-full border-b-2 border-slate-200 ">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 sm:py-2 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -53,21 +53,27 @@ export function Navbar() {
           </div>
 
           <div className="right-0 flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            Oscar Ox
+            <div className="text-4xl font-bold text-slate-900">Oscar Ox</div>
           </div>
 
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
                 <Link href="/">
-                  <a className="rounded-md bg-gray-200 px-3 py-2 text-lg font-medium text-gray-700 hover:bg-gray-700 hover:text-white">
-                    Link
+                  <a className="rounded-md  border-2 border-slate-200 px-3 py-2 text-lg font-medium text-slate-900 hover:border-slate-900">
+                    Calander
                   </a>
                 </Link>
 
                 <Link href="/">
-                  <a className="rounded-md bg-gray-200 px-3 py-2 text-lg font-medium text-gray-700 hover:bg-gray-700 hover:text-white">
-                    Link
+                  <a className="rounded-md  border-2 border-slate-200 px-3 py-2 text-lg font-medium text-slate-900 hover:border-slate-900">
+                    Productions
+                  </a>
+                </Link>
+
+                <Link href="/">
+                  <a className="rounded-md border-2 border-transparent bg-slate-100 px-3 py-2 text-lg font-medium text-slate-900 hover:border-slate-900">
+                    Account
                   </a>
                 </Link>
               </div>
