383e31b1a5b0fcd2b8ae5854b73f5d7d5d391461
AW <atwest40@gmail.com>
2022-09-22 23:22:50 +0100

    Add types/constants to reduce repetition

--- apps/web/src/modules/containers/Navbar/navbar.tsx
+++ apps/web/src/modules/containers/Navbar/navbar.tsx
@@ -2,8 +2,40 @@ import Link from "next/link";
 
 import { useToggle } from "../../../common/hooks";
 import { useUser } from "../../../common/hooks/user";
+import { JoinClassNames } from "../../../common/utils/style";
 
-export function Navbar() {
+type NavbarLinkProps = {
+  title: string;
+  url: string;
+  active: boolean;
+}
+
+const NavbarLink = ({ title, url, active }: NavbarLinkProps) => (
+  <Link href={url}>
+    <a className={JoinClassNames("rounded-md  border-2 border-slate-200 px-3 py-2 text-lg font-medium", (active ? "bg-blue-500 text-slate-600 hover:border-slate-600 hover:bg-blue-900" : "text-slate-900 hover:border-slate-900"))}>
+      {title}
+    </a>
+  </Link>
+)
+
+type NavbarLoginLinkProps = {
+  title: string;
+  url: string;
+}
+
+const NavbarLoginLink = ({ title, url }: NavbarLoginLinkProps) => (
+  <Link href={url}>
+    <a className="rounded-md border-2 border-transparent bg-slate-100 px-3 py-2 text-lg font-medium text-slate-900 hover:border-slate-900">
+      {title}
+    </a>
+  </Link>
+)
+
+type NavbarProps = {
+  active_url?: string;
+}
+
+export function Navbar({ active_url }: NavbarProps) {
   const { user } = useUser();
   const [menu, toggleMenu] = useToggle(false);
 
@@ -58,30 +90,15 @@ export function Navbar() {
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
-                <Link href="/">
-                  <a className="rounded-md  border-2 border-slate-200 px-3 py-2 text-lg font-medium text-slate-900 hover:border-slate-900">
-                    Calander
-                  </a>
-                </Link>
 
-                <Link href="/productions">
-                  <a className="rounded-md  border-2 border-slate-200 px-3 py-2 text-lg font-medium text-slate-900 hover:border-slate-900">
-                    Productions
-                  </a>
-                </Link>
+                <NavbarLink title="Calender" url="/calendar" active={"/calendar" == active_url} />
+
+                <NavbarLink title="Productions" url="/productions" active={"/productions" == active_url} />
 
                 {user.loggedIn ? (
-                  <Link href="/account">
-                    <a className="rounded-md border-2 border-transparent bg-slate-100 px-3 py-2 text-lg font-medium text-slate-900 hover:border-slate-900">
-                      Account
-                    </a>
-                  </Link>
+                  <NavbarLoginLink title="Account" url="/account" />
                 ) : (
-                  <Link href="/login">
-                    <a className="rounded-md border-2 border-transparent bg-slate-100 px-3 py-2 text-lg font-medium text-slate-900 hover:border-slate-900">
-                      Login
-                    </a>
-                  </Link>
+                  <NavbarLoginLink title="Login" url="/login" />
                 )}
               </div>
             </div>
