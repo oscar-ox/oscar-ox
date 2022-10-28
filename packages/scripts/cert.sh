8f0e762d706db1cc626d396a4ad40cc65b92d236
Nathan Rignall <rignallnathan@gmail.com>
2022-10-28 15:38:32 +0100

    Use correct cert name

--- packages/scripts/cert.sh
+++ packages/scripts/cert.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo mkdir -p /etc/docker/certs.d/registry.k8s1.flat.nathanrignall.uk:5000
-sudo cp ca.crt /etc/docker/certs.d/registry.k8s1.flat.nathanrignall.uk:5000/ca.crt
+sudo cp ca.pem /etc/docker/certs.d/registry.k8s1.flat.nathanrignall.uk:5000/ca.crt
