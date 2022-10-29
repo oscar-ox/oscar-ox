017fd95a6be9bf71725641fd92e35f37e7c55170
Nathan Rignall <rignallnathan@gmail.com>
2022-10-29 23:26:52 +0100

    Add tracking and fix ingress

--- apps/web/src/modules/containers/Header/header.tsx
+++ apps/web/src/modules/containers/Header/header.tsx
@@ -8,6 +8,7 @@ export function Header({ title }: Props) {
   return (
     <Head>
       <title>{title}</title>
+      <script async defer data-website-id="01b2b9a6-0e7c-42d7-980f-80af4b147deb" src="https://analytics.nlr.app/umami.js"></script>
     </Head>
   );
 }
